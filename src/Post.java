06967c10b86a278bc68727c9c6703dd3fd674b8e
Alessandro Scala <alessandro.scala8@gmail.com>
2020-11-16 10:21:57 +0100

    Started writing docs

--- src/Post.java
+++ src/Post.java
@@ -2,13 +2,36 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Definition of the immutable Post class. A Post object represents a post of the social network MicroBlog, which is
+ * made up of four data fields:
+ * - String id:			A unique string that identifies the post.
+ * - String author:		A string identifying the user who published the Post.
+ * - String text:		The text the user wrote in the post.
+ * - long timestamp:	A timestamp in UNIX format of the time the Post was created.
+ *
+ * Representation Invariant:
+ *   ((id != null) && (id.length() > 1) && ((author != null) && (author.length() > 1) && ((text != null) && (0 < text.length() <= 140))
+ */
+
 public class Post {
 	private final String id;
 	private final String author;
 	private final String text;
 	private final long timestamp;
 
-
+	/**
+	 * Class constructor.
+	 *
+	 * @requires	author != null && author.length() > 1
+	 * 				text != null && 0 < text.length() <= 140
+	 *
+	 * @exception InvalidUsernameException if the author conditions aren't met
+	 * @exception InvalidPostTextException if the post text conditions aren't met
+	 *
+	 * @param author
+	 * @param text
+	 */
 	public Post(String author, String text) throws InvalidUsernameException, InvalidPostTextException {
 		if(author == null || author.length() < 1){
 			throw new InvalidUsernameException("Called Post constructor with null/empty author");
@@ -16,6 +39,7 @@ public class Post {
 		if(text == null || text.length() < 1){
 			throw new InvalidPostTextException("Called Post constructor with null text");
 		}
+		//TODO: Check text.length() <= 10
 
 		this.author = author;
 		this.text = text;
@@ -23,13 +47,11 @@ public class Post {
 		this.id = timestamp + author;
 	}
 
+	/**
+	 * Checks if the post text contains at least one of the words passed as parameter.
+	 *
 
-	/*private Post(Post post){
-		this.id = post.id;
-		this.author = post.author
-	}*/
-
-
+	 */
 	public boolean containsAny(List<String> words){
 		if(words == null || words.size() == 0){
 			return true;
